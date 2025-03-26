47c2148049d2f328f204d109d8c4b99e03f84389
Cintia Cabral <cintiacabral@hotmail.com>
2025-03-26 16:10:48 -0300

    Configuring db, and first route for payment creation

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+from payment_api.database import db
+from models.payments import Payment
+from datetime import datetime, timedelta
+from payments.pix import Pix
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+db.init_app(app)
+
+@app.route('/payments/pix', methods=['POST'])
+def create_payment_pix():
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({"message":"Invalid value"}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify ({"message":"The payment has been created",
+                     "payment": new_payment.to_dict()})
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+    return jsonify ({"message":"The payment has been confirmed"})
+
+@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+def payment_pix_page(payment_id):
+    return 'pix payment'
+
+if __name__=='__main__':
+    app.run(debug=True)
